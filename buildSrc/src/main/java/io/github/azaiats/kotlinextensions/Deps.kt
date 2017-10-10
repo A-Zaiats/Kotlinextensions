ea2762c7868cfb671e21ced0c7f53256ac0cbc6a
Andrei Zaiats <andrei.zaiats@gmail.com>
2017-10-10 20:52:50 +0300

    Reorganize configs.

---
+++ buildSrc/src/main/java/io/github/azaiats/kotlinextensions/Deps.kt
@@ -0,0 +1,11 @@
+@file:JvmName("Deps")
+
+package io.github.azaiats.kotlinextensions
+
+const val PLUGIN_GRADLE = "com.android.tools.build:gradle:$GRADLE_PLUGIN_VERSION"
+const val PLUGIN_KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+const val PLUGIN_BINTRAY = "com.novoda:bintray-release:$BINTRAY_PLUGIN_VERSION"
+
+const val KOTLIN_STD = "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION"
+const val SUPPORT_APPCOMPAT = "com.android.support:appcompat-v7:$ANDROID_SUPPORT_VERSION"
+const val SUPPORT_DESIGN = "com.android.support:design:$ANDROID_SUPPORT_VERSION"
